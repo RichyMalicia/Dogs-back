37d9c7e2fcd32412aade857db9e0e72ebbb762d5
RichyMalicia <97110478+RichyMalicia@users.noreply.github.com>
2022-09-09 09:27:13 -0300

    make it better

--- index.js
+++ index.js
@@ -19,13 +19,11 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const {
-  PORT
-} = process.env;
+
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(PORT, () => {
+  server.listen(process.env.PORT || 3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
